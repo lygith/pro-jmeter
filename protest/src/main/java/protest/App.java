69a11892d63224660406c4f37c678e058ecad084
ncs <liuyu@ncsi.com.cn>
2017-03-31 10:02:35 +0800

    add new

---
+++ protest/src/main/java/protest/App.java
@@ -0,0 +1,13 @@
+package protest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
